649f04086a4e3e15a7543ec4d0ec908e0b8b34b0
Almir <almir.almiev@tatar.ru>
2019-10-26 16:12:55 +0300

    Formated

--- src/main/java/ru/itpark/comparator/ProductByIdAscComparator.java
+++ src/main/java/ru/itpark/comparator/ProductByIdAscComparator.java
@@ -1,13 +1,14 @@
 package ru.itpark.comparator;
 
 import ru.itpark.model.Product;
-
 import java.io.Serializable;
 import java.util.Comparator;
+
+import static java.lang.Math.*;
 
 public class ProductByIdAscComparator implements Comparator<Product>, Serializable {
     @Override
     public int compare(Product o1, Product o2) {
-        return Math.toIntExact(o1.getId() - o2.getId());
+        return toIntExact(o1.getId() - o2.getId());
     }
 }
